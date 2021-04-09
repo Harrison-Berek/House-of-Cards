64c6824c8579bcca6aeb2caff4e9ac9a5c6310fa
Harrison-Berek <hcberek@gmail.com>
2021-04-09 16:12:59 -0600

    Index Page

--- houseofcards/main_app/urls.py
+++ houseofcards/main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('games/', views.games_index, name='index'),
 ]
--- houseofcards/main_app/views.py
+++ houseofcards/main_app/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
+class Games:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name):
+    self.name = name
+
+games = [
+  Games('Spades'),
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+def games_index(request):
+    return render(request, 'games/index.html', {
+        'games': games 
+    })
