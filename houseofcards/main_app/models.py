8a3da4bee01dc2db3e31744dd33532ede8d019b7
Harrison-Berek <hcberek@gmail.com>
2021-04-12 16:50:45 -0600

    Create Function

--- houseofcards/main_app/models.py
+++ houseofcards/main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Game(models.Model):
@@ -7,11 +8,11 @@ class Game(models.Model):
   rules = models.TextField(max_length=5000)
   region = models.CharField(max_length=100)
   num_player_choices = (
-    (1, "1"),
-    (2, "2"),
-    (3, "3"),
-    (4, "4"),
-    (5, "5+"),
+    ("1", "1"),
+    ("2", "2"),
+    ("3", "3"),
+    ("4", "4"),
+    ("5", "5+"),
   )
   num_players = models.CharField(
     max_length= 20,
@@ -23,3 +24,6 @@ class Game(models.Model):
 
   def __str__(self):
     return self.name
+
+  def get_absolute_url(self):
+    return reverse('index')
--- houseofcards/main_app/views.py
+++ houseofcards/main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Game
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -6,9 +8,15 @@ def home(request):
   return render(request, 'home.html')
 
 def games_index(request):
+  games = Game.objects.all()
   return render(request, 'games/index.html', {
     'games': games 
     })
+
+class GamesCreate(CreateView):
+  model = Game
+  fields = '__all__'
+
 
 # def favorites_index(request):
 #   return render(request, 'favorites/index.html', {
